a453832e69eb4b86a2e8156de376b0491c63f0a4
jimm977 <33897796+jimm977@users.noreply.github.com>
2018-01-24 22:58:40 +0300

    2
    Messed resolution at first (400 instead of 480)

--- plot1.R
+++ plot1.R
@@ -5,13 +5,15 @@ dTab<-read.table(file="i:\\CourserA\\DATA_SCIENCE\\04_Exploratoty_research\\Wk1\
           col.names = c("Date","Time","Gl_act_power","Gl_react_power","Voltage","Gl_intensity","Sub_met1","Sub_met2","Sub_met3"),
           numerals="no.loss",
           nrows=2*24*60, skip = 66637)
+
+Sys.setlocale("LC_TIME", "C")
 dTab$Date<-as.Date(dTab$Date, "%d/%m/%Y")
 dTab$Time<-as.POSIXct(strptime(paste(dTab$Date,dTab$Time),"%Y-%m-%d %H:%M:%S"))
 
 ## Plotting part
-png(file="i:\\CourserA\\_RStudio_WorkingDir\\1\\plot1.png", width = 400, height = 400)
+png(file="i:\\CourserA\\_RStudio_WorkingDir\\1\\plot1.png", width = 480, height = 480)
 
 hist(dTab$Gl_act_power, breaks=12, col = "red", main="Global active power", 
-     xlab="Global active power (kilowatts)",xlim=c(0,6),ylim=c(0,1400))
+     xlab="Global active power (kilowatts)")
 
 dev.off()
--- plot3.R
+++ plot3.R
@@ -5,17 +5,17 @@ dTab<-read.table(file="i:\\CourserA\\DATA_SCIENCE\\04_Exploratoty_research\\Wk1\
           col.names = c("Date","Time","Gl_act_power","Gl_react_power","Voltage","Gl_intensity","Sub_met1","Sub_met2","Sub_met3"),
           numerals="no.loss",
           nrows=2*24*60, skip = 66637)
+Sys.setlocale("LC_TIME", "C")
 dTab$Date<-as.Date(dTab$Date, "%d/%m/%Y")
 dTab$Time<-as.POSIXct(strptime(paste(dTab$Date,dTab$Time),"%Y-%m-%d %H:%M:%S"))
 
 ## Plotting part
-png(file="i:\\CourserA\\_RStudio_WorkingDir\\1\\plot3.png", width = 400, height = 400)
+png(file="i:\\CourserA\\_RStudio_WorkingDir\\1\\plot3.png", width = 480, height = 480)
 
 plot(dTab$Time, dTab$Sub_met1, type="l", xlab="", ylab="Energy sub metering")
 lines(dTab$Time, dTab$Sub_met2, col="red")
 lines(dTab$Time, dTab$Sub_met3, col="blue")
 legend('topright',legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"),
        lty = c(1,1,1))
-
 
 dev.off()
